e7e29e376a8db38ea6dca7c856722231a0e12445
sudenazbuyukcukurlu <sudenazbuyukcukurlu@gmail.com>
2024-08-10 18:25:27 +0300

    Katmanlar oludturulmaya baslandi

---
+++ Asp.netcore-projesi/Asp.netcore-projesi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Asp.netcore_projesi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Asp.netcore-projesi/EntityLayer/Concrete/Feature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Feature
+    {
+        public int FeatureID { get; set; }
+        public string Header  { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+
+    }
+}
