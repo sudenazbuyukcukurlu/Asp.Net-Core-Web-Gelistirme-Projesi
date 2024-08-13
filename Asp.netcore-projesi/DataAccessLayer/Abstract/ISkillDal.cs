399701d20fedcfbcf5a3a03d68601db23f7b9950
sudenazbuyukcukurlu <sudenazbuyukcukurlu@gmail.com>
2024-08-13 21:20:57 +0300

    Interfacelere geçildi

---
+++ Asp.netcore-projesi/DataAccessLayer/Abstract/ISkillDal.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    interface ISkillDal
+    {
+        void Insert (Skill p);
+        void Delete (Skill p);
+        void Update (Skill p);
+        List<Skill> GetList();
+    }
+}
