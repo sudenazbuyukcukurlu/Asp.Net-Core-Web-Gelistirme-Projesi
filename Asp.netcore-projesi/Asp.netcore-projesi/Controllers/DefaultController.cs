8b7fea9f14b3fe34a6754927d6a5006e6ca0152d
sudenazbuyukcukurlu <sudenazbuyukcukurlu@gmail.com>
2024-09-02 10:42:37 +0300

    tema düzenlendi kontrol edilmedi

---
+++ Asp.netcore-projesi/Asp.netcore-projesi/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.netcore_projesi.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
