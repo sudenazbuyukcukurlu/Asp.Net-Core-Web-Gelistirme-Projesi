aeb57df74e3570fae2739323a75230d5f78bd5c6
sudenazbuyukcukurlu <sudenazbuyukcukurlu@gmail.com>
2024-09-02 16:33:23 +0300

    view components eklendi

---
+++ Asp.netcore-projesi/Asp.netcore-projesi/ViewComponents/Feature/FeatureList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.netcore_projesi.ViewComponents.Feature
+{
+    public class FeatureList: ViewComponent
+    {
+        public IViewComponentResult Invoke( )
+        {
+            return View();
+        }
+    }
+}
